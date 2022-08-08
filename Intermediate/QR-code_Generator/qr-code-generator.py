3bbc9262865b64b1a993519111c549b2653083df
Aswin KS <108337342+aswinks1995@users.noreply.github.com>
2022-08-08 12:21:02 +0530

    Update qr-code-generator.py

--- Intermediate/QR-code_Generator/qr-code-generator.py
+++ Intermediate/QR-code_Generator/qr-code-generator.py
@@ -6,17 +6,17 @@ from PIL import Image
 
 #Asking for data
 x=input("Enter the URL or text to convert to QR code: ")
-if x == "": #If user didnt give an inout then choose default value.
+if x == "": #If user didnt give an input then choose default value.
     #Optional session: This will create the QR code with default URL!
 
     x="https://github.com/aswinks1995"
-    print("No data provided. Using default URl to create QRcode!")
+    print("No data provided. Using default URL to create QRcode!")
     img = qrcode.make(x)
     img.save("qr-image.jpg")
     print("QR-code saved!")
    #-----------------------------------------------------------------
 
-else:#If user provide input then create qr code with user's input
+else:#If user provide input,then create qr code with user's input
     img = qrcode.make(x)
 
     img.save("qr-image.jpg")
